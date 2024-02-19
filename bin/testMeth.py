64602a878fa7087db3126d69d1c8ada4a03b1982
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2024-02-20 01:47:40 +0530

    Indian Names Dataframe generated

--- bin/testMeth.py
+++ bin/testMeth.py
@@ -1,3 +1,8 @@
+import os
+import sys
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
 import findspark
 
 findspark.find()
@@ -7,6 +12,7 @@ from pyspark.sql import SparkSession, SQLContext
 from pyspark.context import SparkContext
 from pyspark.sql.functions import * 
 from pyspark.sql.types import * 
+
 
 def initSpark():
     spark = SparkSession.builder.master('local[1]').appName('Covid-dummy-test').getOrCreate()
@@ -24,7 +30,7 @@ def filterNames(s):
             return x
         else :
             return ''
-    except ex :
+    except :
         # print('Something went wrong')
         pass
     else :
@@ -33,16 +39,54 @@ def filterNames(s):
     
 
 def findMostNames(spark, filePath):
-    df = spark.read.option('header',True).csv(filePath)
+    df = spark.read.option('inferSchema',True).option('header',True).csv(filePath)
     df.show( truncate=False)
-
+    return df
 
 def testMethodExec():
     spark = initSpark()
-    findMostNames(spark,'./data/sourceData.csv')
-
+    
+    return spark 
 if __name__=='__main__':
-    # testMethodExec()
+    spark = testMethodExec()
+    df = findMostNames(spark,'./data/indian-male-names.csv')
+    df.printSchema()
+    # df.show(30)
     print(filterNames("_12_اف_"))
     print(filterNames("James Ford"))
     print(filterNames('कु0'))
+    print(filterNames(''))
+    # df2 = df.filter(df.name.isNull())
+    # print(df2.count())
+    # df2.show()
+    
+    validateNameUDF = udf(lambda f : filterNames(f) , StringType())
+    # df.show()
+    # df.dropna()
+    df = df.withColumn('firstName', when(df.name.isNull(),"").otherwise(validateNameUDF(df.name)) )
+    
+    # df.show(10)
+    df = df.dropna()
+    df = df.select('firstName','gender')
+
+    df2 = findMostNames(spark,'./data/indian-female-names.csv')
+    df2.printSchema()
+    df2 = df2.withColumn('firstName', when(df2.name.isNull(),"").otherwise(validateNameUDF(df2.name)) )
+    df2 = df2.dropna()
+    df2 = df2.select('firstName','gender')
+
+    df = df.union(df2)
+    print('Female count : ',df.filter(df.gender=='f').count())
+    print('Male count : ',df.filter(df.gender=='m').count())
+    
+
+    df = df.repartition(10)
+    df = df.distinct()
+    print('Female count : ',df.filter(df.gender=='f').count())
+    print('Male count : ',df.filter(df.gender=='m').count())
+    
+    df.show(50)
+    df = df.coalesce(1)
+    df.write.mode('overwrite').option('delimiter', '|').option('header', True).csv('ind-names')
+
+    print('Write complete ')
