144843f55bcc86fa1ea2fc0d5f68410092d39ffe
kmth-hash <bpk.san4ya@gmail.com>
2024-07-09 00:40:47 +0530

    Kafka Producer init setup complete

--- bin/dashboard/sourceDataset.py
+++ bin/dashboard/sourceDataset.py
@@ -5,11 +5,15 @@ from datetime import datetime
 import requests
 import os
 from dotenv import load_dotenv
+from kafka import KafkaProducer
 
 location = ['Pune', 'Mumbai', 'Bangalore',
             'Chennai', 'Gurgaon', 'Kolkata', 'Hyderabad']
 currFilePath = os.path.abspath(__file__)
-currPath = '\\'.join(currFilePath.split('\\')[:-1])+str('\\.env')
+# Linux machine --> '/'
+# Windows Machine --> '\\'
+# env file in the same dir as this script . Rename .env.templete to .env with proper values
+currPath = '/'.join(currFilePath.split('/')[:-1])+str('/.env')
 currPath = currPath[0].upper()+currPath[1:]
 load_dotenv(currPath)
 
@@ -39,25 +43,39 @@ def getDataFromAPI(apiKey, location):
     return (json.dumps(response))
 
 
-def sourceStream():
+def sourceStream(producer,topicName):
     def getData(src='local'):
         if src == 'local':
             # Replicating API to generate dummy data for streaming as source/producer
 
             while True:
-                print(generateDataLocally())
+                res = generateDataLocally()
+                print(res)
+                producer.send(topicName,res)
                 time.sleep(random())
         else:
             # Fetch data from API based on location
 
             ind = 0
             ln = len(location)
+            apiKey = os.getenv('APIKEY')
             while True:
-                print(getDataFromAPI(os.getenv('APIKEY'), location[ind]))
+                producer.send(topicName,getDataFromAPI(apiKey, location[ind]))
                 ind += 1
                 if ind == ln:
                     ind = 0
     getData('local')
 
+def initProducer():
+    topicName = os.getenv('topicNAME')
+    bootstrapServer = [os.getenv('bootstrapServer')]
+    print(topicName , bootstrapServer)
+    producer = KafkaProducer(bootstrap_servers=bootstrapServer , value_serializer=lambda x : json.dumps(x).encode('utf-8'))
+    sourceStream(producer,topicName)
 
-sourceStream()
+
+    # Test this code usin Kafka Consumer in a separate terminal
+    # bin/kafka-console-consumer.sh --topic <topic_name> --bootstrap-server <server | localhost:9092 >
+    #Streams of data will be printed in consumer terminal 
+
+initProducer()
